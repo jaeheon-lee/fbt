0f52777a164a530880164e09b7d280bee988c392
kjy <wpdud001@gmail.com>
2020-10-27 17:09:52 +0900

    [2020-10-27 17:09] 충돌 해결2

--- DB modeling/DB Query/DB_Select.sql
+++ DB modeling/DB Query/DB_Select.sql
@@ -75,44 +75,5 @@ ON v.team_id = tm.team_id
 GROUP BY vr.vote_match_id
 ;
 
-select * from vote_match_result;
-
-SELECT 
-v.vote_match_id, v.vote_reg_date, v.due_date,  
-m.start_time, m.duration, m.stadium_address, m.match_type,
-m.cost, m.stadium_parking, m.content,
-vr.attendance, vr.team_member_id, vr.email , tm.total_num,
-vs.type, vs.friend_emp,
-t.team_name, t.emblem
-FROM match_schedule m
-JOIN vote_match v
-ON v.match_schedule_id = m.match_schedule_id
-LEFT OUTER join vote_match_result vr
-on v.vote_match_id = vr.vote_match_id
-JOIN (
-SELECT team_id, count(team_id) total_num
-FROM team_member
-) tm
-ON tm.team_id=v.team_id
-JOIN vote_match_setting vs
-ON v.vote_match_id = vs.vote_match_id
-JOIN team t
-ON v.team_id = t.team_id
-WHERE v.vote_status = 0
-AND v.team_id = 1;
-
-SELECT 
-v.vote_match_id, v.vote_reg_date, v.due_date, 
-m.start_time, m.duration, m.stadium_address, m.match_type,
-m.cost, m.stadium_parking, m.content,
-t.emblem, t.team_name
-FROM vote_match v
-JOIN match_schedule m
-ON v.match_schedule_id = m.match_schedule_id
-JOIN team t 
-ON m.home_team_id = t.team_id;
-
-SELECT
-
         
 
